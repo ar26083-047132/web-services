fb1d721f13fca50983b038ca2593dd1f00c3dbe9
mukhtarA <m.abulmansur32@gmail.com>
2021-10-12 14:25:38 +0600

    feat: add actuator

---
+++ src/main/java/com/admin/stock/StockApplication.java
@@ -0,0 +1,16 @@
+package com.admin.stock;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class StockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockApplication.class, args);
+	}
+
+
+}
